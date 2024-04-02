6e8a5eb1f503847231069ef09b748c38fe9d8894
Lucas Moraes <lucckasmoraes@gmail.com>
2024-04-02 20:05:47 -0300

    Criando E-commerce

--- jQuery/js/custom.js
+++ jQuery/js/custom.js
@@ -7,4 +7,17 @@ jQuery(function($){
     let destaques = $('#featured') // id
     
     console.log(titulos.first()); //trabalhando desta forma evita conflitos, caso esteja trabalhando com varias bibliotecas.
+
+    // Configuração de produtos
+
+    $('.featured-item a').addClass('btn btn-dark strech-link');
+    // $('.featured-item:first h4').star('<span class="badge bg-secondary">Novo</span>')
+    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
+    // $('.featured-item:first h4').addClass('active')
+    // $('.featured-item:first h4').removeClass('active')
+    // $('.featured-item:first h4').hide()
+    // $('.featured-item:first h4').show()
+    // $('.featured-item:first h4').fade()
+     
+    
 })
