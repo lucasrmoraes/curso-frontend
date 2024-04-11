56461411a428d56c546b33025d831f3810af0e02
Lucas Moraes <lucckasmoraes@gmail.com>
2024-04-11 19:56:06 -0300

    Atualizando diretorio

--- jQuery/verton/js/custom.js
+++ jQuery/verton/js/custom.js
@@ -1,16 +1,21 @@
 // instancia jquery e evita conflitos
+// jQuery(function($){
 
-jQuery(function($){
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
 
     let titulos = $('h4') // tag
+    
     let itens = $('.featured-item') // class
+
     let destaques = $('#featured') // id
     
     console.log(titulos.first()); //trabalhando desta forma evita conflitos, caso esteja trabalhando com varias bibliotecas.
 
     // Configuração de produtos
 
-    $('.featured-item a').addClass('btn btn-dark strech-link');
+    // $('.featured-item a').addClass('btn btn-dark strech-link');
     // $('.featured-item:first h4').star('<span class="badge bg-secondary">Novo</span>')
     // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
     // $('.featured-item:first h4').addClass('active')
